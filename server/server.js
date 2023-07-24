4f197b86275f6acae9486dc5b3b9f16c8c588413
minalamit <lamitcarmina@gmail.com>
2023-07-24 16:06:01 +0800

    finished routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,71 @@
-// modules
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-// middlewares
-const app = express()
+// Get all Restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+  console.log("route handler ran");
 
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["mcdonalds", "wendys"],
+    },
+  });
+});
 
+//Get a Restaurant
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params);
 
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
 
-const port = 5000
-app.listen(5000, () => {
-    console.log(`Server is running on PORT ${port}!`)
-})
+// Create a Restaurant
+app.post("/api/v1/restaurants", async (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
+
+// Update Restaurants
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
+
+// Delete Restaurant
+
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}!`);
+});
