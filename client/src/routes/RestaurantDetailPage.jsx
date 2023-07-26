76ff215c25fb1755bff2aad23ab489df69dbdd72
minalamit <lamitcarmina@gmail.com>
2023-07-26 22:40:27 +0800

    added routes

---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RestaurantDetailPage() {
+    return (
+        <>RestaurantDetailPage</>
+    )
+}
+
+export default RestaurantDetailPage;
---
+++ client/src/routes/UpdatePagee.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function UpdatePagee() {
+    return (
+        <>UpdatePage</>
+    )
+}
+
+export default UpdatePagee;
